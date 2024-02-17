70d27e2b354e5a7ff6f51beae04c3ebd933a7f3e
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-02-17 14:40:05 -0500

    update

--- NYT Crossword.user.js
+++ NYT Crossword.user.js
@@ -2,10 +2,23 @@
 // @name         NYT Crossword
 // @namespace    https://joshr.work/
 // @homepageURL  https://joshr.work/
-// @version      1.0.3
+// @version      1.0.4
 // @author       Josh
 // @match        *://*.nytcrosswordanswers.org/*
 // @icon         https://www.youtube.com/s/desktop/54055272/img/favicon.ico
 // @require      https://raw.githubusercontent.com/joshrouwhorst/userscripts/main/Utils.js
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // ==/UserScript==
+
+const { Log, OnLocationChange, RemoveAds } = Utils
+
+$(() => {
+  OnLocationChange(() => run())
+})
+
+function run() {
+  Log('NYT Crossword User Script Running...')
+  RemoveAds(['.adsbygoogle'])
+
+  $('.entry-content').remove(':not(.answ)')
+}
