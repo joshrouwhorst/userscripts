f8eab77528c1ffc6ceecd98f42ba3e73493772db
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-02-23 20:36:31 -0500

    update

--- StackOverflow.user.js
+++ StackOverflow.user.js
@@ -6,6 +6,7 @@
 // @author       Josh
 // @match        *://*.stackoverflow.com/*
 // @require      https://raw.githubusercontent.com/joshrouwhorst/userscripts/main/Utils.js
+// @icon         https://cdn2.iconfinder.com/data/icons/social-icons-color/512/stackoverflow-1024.png
 // ==/UserScript==
 
 if (jk_DEBUG('stack.overflow')) debugger
