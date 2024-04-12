93f70cfda1d57299d9328ada6039e101406e49f0
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-04-12 13:44:45 -0400

    update

--- Bible Gateway.user.js
+++ Bible Gateway.user.js
@@ -2,7 +2,7 @@
 // @name         Bible Gateway
 // @namespace    https://joshr.work/
 // @homepageURL  https://joshr.work/
-// @version      1.0.44
+// @version      1.0.45
 // @author       Josh
 // @match        *://*.biblegateway.com/passage/*
 // @icon         https://biblegateway.com/favicon.ico
--- Wikipedia.user.js
+++ Wikipedia.user.js
@@ -2,7 +2,7 @@
 // @name         Wikipedia
 // @namespace    https://joshr.work/
 // @homepageURL  https://joshr.work/
-// @version      1.0.44
+// @version      1.0.45
 // @author       Josh
 // @match        *://*.wikipedia.org/*
 // @icon         https://www.wikipedia.org/static/favicon/wikipedia.ico
