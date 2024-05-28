78877cfc7d9a5d5671c7631abb1d352662ae37fe
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-05-28 12:14:03 -0400

    update

--- Bible Gateway.user.js
+++ Bible Gateway.user.js
@@ -2,7 +2,7 @@
 // @name         Bible Gateway
 // @namespace    https://joshr.work/
 // @homepageURL  https://joshr.work/
-// @version      1.1.14
+// @version      1.1.15
 // @author       Josh
 // @match        *://*.biblegateway.com/passage/*
 // @icon         https://biblegateway.com/favicon.ico
