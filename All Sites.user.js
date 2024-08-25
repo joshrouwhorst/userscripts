9fe62ee1b6f9c564596c6bef1d92e99e23d2f0f6
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-08-25 18:28:42 -0400

    update

--- All Sites.user.js
+++ All Sites.user.js
@@ -2,7 +2,7 @@
 // @name         All Sites
 // @namespace    https://joshr.work/
 // @homepageURL  https://github.com/joshrouwhorst/userscripts/raw/main/All%20Sites.user.js
-// @version 1.1.43
+// @version 1.1.44
 // @author       Josh
 // @match        *://*/*
 // @require      https://raw.githubusercontent.com/joshrouwhorst/userscripts/main/_JackKnife.js
@@ -31,7 +31,16 @@ Load(() => {
   addBar()
   removeCookieBanners()
   siteInfo()
+  reminders()
 })
+
+function reminders() {
+  Log('REMINDERS:')
+  Log(
+    '- Add `jkbar=true` to the URL to show the JackKnife bar or `jkbar=false` to hide'
+  )
+  Log('- Add `show.banners=true` to the URL to show cookie banners')
+}
 
 function addBar() {
   AddButton('Stop Reload', () => {
