aec4eb5c973af4bfc0c512474a8a47ac18f015b8
Josh Rouwhorst <joshrouwhorst@users.noreply.github.com>
2024-09-27 14:36:42 -0400

    update

--- All Sites.user.js
+++ All Sites.user.js
@@ -2,7 +2,7 @@
 // @name         All Sites
 // @namespace    https://joshr.work/
 // @homepageURL  https://github.com/joshrouwhorst/userscripts/raw/main/All%20Sites.user.js
-// @version 1.1.55
+// @version 1.1.56
 // @author       Josh
 // @match        *://*/*
 // @downloadURL  https://raw.githubusercontent.com/joshrouwhorst/userscripts/main/All%20Sites.user.js
--- _JackKnife.js
+++ _JackKnife.js
@@ -8,28 +8,36 @@ function jk_StandardizeElems(elems) {
   return elems
 }
 
-const JackKnife = {
+// Effectively make JackKnife a singleton
+const JackKnife = new JackKnifeInstance()
+
+class JackKnifeInstance {
   Log(text) {
     console.log(`%cUS | ${text}`, 'font-weight: bold;')
-  },
+  }
+
   Err(text) {
     console.error(
       `%cUS | ${text}`,
       'font-weight: bold; background-color: red; color: white;'
     )
-  },
+  }
+
   Loop(interval, func) {
     // Return false to stop the loop
     var keepGoing = func() || true // Default to true
     if (keepGoing) setTimeout(() => this.Loop(interval, func), interval)
-  },
+  }
+
   Memory(key, value) {
     if (value) localStorage.setItem(key, value)
     return localStorage.getItem(key)
-  },
+  }
+
   Obj(object) {
     console.log(object)
-  },
+  }
+
   GetQueryVariable(variable) {
     try {
       let query = null
@@ -57,7 +65,8 @@ const JackKnife = {
     }
 
     return null
-  },
+  }
+
   GetAriaLabel(elem) {
     const label = elem.getAttribute('aria-label')
     if (label) return label
@@ -66,13 +75,15 @@ const JackKnife = {
     if (id) return document.getElementById(id).textContent
 
     return null
-  },
+  }
+
   HasParam(name) {
     return (
       window.location.search.indexOf(name) > -1 ||
       window.location.hash.indexOf(name) > -1
     )
-  },
+  }
+
   OnLocationChange(func) {
     try {
       const observeUrlChange = () => {
@@ -95,7 +106,8 @@ const JackKnife = {
     }
 
     func(document.location.href)
-  },
+  }
+
   HandleAds(selectors, loopTime, each) {
     if (!loopTime) loopTime = 500
     selectors.forEach((selector) => {
@@ -107,23 +119,27 @@ const JackKnife = {
     })
 
     setTimeout(() => JackKnife.RemoveAds(selectors, loopTime), loopTime)
-  },
+  }
+
   RemoveAds(selectors, loopTime) {
     return JackKnife.HandleAds(selectors, loopTime, (element) => {
       element.remove()
     })
-  },
+  }
+
   HideAds(selectors, loopTime) {
     return JackKnife.HandleAds(selectors, loopTime, (element) => {
       element.style.visibility = 'hidden'
     })
-  },
+  }
+
   MakeElement(html) {
     // Takes string of HTML and returns a DOM element
     const temp = document.createElement('div')
     temp.innerHTML = html
     return temp.firstChild
-  },
+  }
+
   Text(elems, text) {
     elems = jk_StandardizeElems(elems)
     if (!text) {
@@ -134,54 +150,64 @@ const JackKnife = {
         elem.textContent = text
       })
     }
-  },
+  }
+
   Hide(elems) {
     elems = jk_StandardizeElems(elems)
     elems.forEach((elem) => {
       elem.style.display = 'none'
     })
-  },
+  }
+
   Show(elems) {
     elems = jk_StandardizeElems(elems)
     elems.forEach((elem) => {
       elem.style.display = ''
     })
-  },
+  }
+
   Remove(elems) {
     elems = jk_StandardizeElems(elems)
     elems.forEach((elem) => {
       elem.remove()
     })
-  },
+  }
+
   Replace(oldElem, newElem) {
     oldElem.parentNode.replaceChild(newElem, oldElem)
-  },
+  }
+
   Before(reference, newItem) {
     reference = jk_StandardizeElems(reference)
     reference.forEach((ref) => {
       ref.parentNode.insertBefore(newItem, ref)
     })
-  },
+  }
+
   After(reference, newItem) {
     reference = jk_StandardizeElems(reference)
     reference.forEach((ref) => {
       if (ref.nextSibling) ref.parentNode.insertBefore(newItem, ref.nextSibling)
       else ref.parentNode.appendChild(newItem)
     })
-  },
+  }
+
   On(elem, event, func) {
     elem.addEventListener(event, func)
-  },
+  }
+
   Trigger(elems, event) {
     elems = jk_StandardizeElems(elems)
     elems.forEach((elem) => {
       elem.dispatchEvent(new Event(event))
     })
-  },
+  }
+
   Is(elems, selector) {
     elems = jk_StandardizeElems(elems)
     return elems.every((elem) => elem.matches(selector))
-  },
+  }
+
   CSS(elems, styles) {
     elems = jk_StandardizeElems(elems)
     elems.forEach((elem) => {
@@ -189,23 +215,26 @@ const JackKnife = {
         elem.style[prop] = styles[prop]
       }
     })
-  },
+  }
+
   Load(func) {
     if (document.readyState !== 'loading') return func()
     document.addEventListener('DOMContentLoaded', func)
-  },
+  }
+
   Select(selector, parent) {
     if (parent) {
       return parent.querySelectorAll(selector) || []
     } else {
       return document.querySelectorAll(selector) || []
     }
-  },
+  }
+
   $(selector, parent) {
     if (parent) {
       return parent.querySelectorAll(selector) || []
     } else {
       return document.querySelectorAll(selector) || []
     }
-  },
+  }
 }
